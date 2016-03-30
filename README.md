9544bf9fdae553019b6252bced23759f13c980fb
Adrien Siffermann <adrien.siffermann_ext@bpce.fr>
2016-03-31 01:25:26 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # summernote-image-title
-Summernote plugin to edit an image title and alt attributes
+A plugin for the [Summernote](https://github.com/summernote/summernote/) WYSIWYG editor.
+
+Adds a button to the image popover to edit title and alt attributes.
+
+### Installation
+
+#### 1. Include JS
+
+Include the following code after Summernote:
+
+```html
+<script src="summernote-image-title.js"></script>
+```
+
+#### 2. Include language file
+
+Eventually include language file (currently available in French, contributions welcomed!)
+
+```html
+<script src="lang/summernote-image-title-fr-FR.js"></script>
+```
+
+#### 3. Summernote options
+
+Finally, customize the Summernote image popover:
+
+```javascript
+$(document).ready(function() {
+    $('#summernote').summernote({
+        lang: 'fr-FR',
+        popover: {
+            image: [
+                ['imagesize', ['imageSize100', 'imageSize50', 'imageSize25']],
+                ['float', ['floatLeft', 'floatRight', 'floatNone']],
+                ['remove', ['removeMedia']],
+                ['custom', ['imageTitle']],
+            ],
+        },
+    });
+});
+```
---
+++ summernote-image-title.js
@@ -0,0 +1,129 @@
+﻿(function (factory) {
+    /* global define */
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(window.jQuery);
+    }
+}(function ($) {
+    $.extend($.summernote.lang['en-US'], {
+        imageTitle: {
+            edit: 'Edit title',
+            fieldLabel: 'Title',
+        },
+    });
+
+    $.extend($.summernote.plugins, {
+        'imageTitle': function (context) {
+            var self = this;
+
+            var ui = $.summernote.ui;
+            var $note = context.layoutInfo.note;
+            var $editor = context.layoutInfo.editor;
+            var $editable = context.layoutInfo.editable;
+            var options = context.options;
+            var lang = options.langInfo;
+
+            context.memo('button.imageTitle', function () {
+                var button = ui.button({
+                    contents: ui.icon(options.icons.pencil),
+                    tooltip: lang.imageTitle.edit,
+                    click: function (e) {
+                        context.invoke('imageTitle.show');
+                    }
+                });
+
+                return button.render();
+            });
+
+            this.initialize = function () {
+                var $container = options.dialogsInBody ? $(document.body) : $editor;
+
+                var body = '<div class="form-group note-group-image-title">' +
+                             '<label>' + lang.imageTitle.fieldLabel + '</label>' +
+                             '<input class="note-image-title-text form-control col-md-12" type="text" />' +
+                           '</div>';
+
+                var footer = '<button href="#" class="btn btn-primary note-image-title-btn disabled" disabled>' + lang.imageTitle.edit + '</button>';
+
+                this.$dialog = ui.dialog({
+                    title: lang.imageTitle.edit,
+                    body: body,
+                    footer: footer
+                }).render().appendTo($container);
+            };
+
+            this.destroy = function () {
+                ui.hideDialog(this.$dialog);
+                this.$dialog.remove();
+            };
+
+            this.bindEnterKey = function ($input, $btn) {
+                $input.on('keypress', function (event) {
+                    if (event.keyCode === 13) {
+                        $btn.trigger('click');
+                    }
+                });
+            };
+
+            this.show = function () {
+                var $img = $($editable.data('target'));
+                var imgInfo = {
+                    imgDom: $img,
+                    title: $img.attr('alt'),
+                };
+                this.showLinkDialog(imgInfo).then(function (imgInfo) {
+                    ui.hideDialog(self.$dialog);
+                    var $img = imgInfo.imgDom;
+                    $img.attr('alt', imgInfo.title);
+                    $img.attr('title', imgInfo.title);
+
+                    $note.val(context.invoke('code'));
+                    $note.change();
+                });
+            };
+
+            this.showLinkDialog = function (imgInfo) {
+                return $.Deferred(function (deferred) {
+                    var $imageTitle = self.$dialog.find('.note-image-title-text'),
+                        $editBtn = self.$dialog.find('.note-image-title-btn');
+
+                    ui.onDialogShown(self.$dialog, function () {
+                        context.triggerEvent('dialog.shown');
+
+                        $editBtn.click(function (event) {
+                            event.preventDefault();
+                            deferred.resolve({
+                                imgDom: imgInfo.imgDom,
+                                title: $imageTitle.val(),
+                            });
+                        });
+
+                        $imageTitle.on('keyup paste', function () {
+                            var url = $imageTitle.val();
+                            ui.toggleBtn($editBtn, url);
+                        }).val(imgInfo.title).trigger('focus');
+
+                        self.bindEnterKey($imageTitle, $editBtn);
+                    });
+
+                    ui.onDialogHidden(self.$dialog, function () {
+                        $imageTitle.off('keyup paste keypress');
+                        $editBtn.off('click');
+
+                        if (deferred.state() === 'pending') {
+                            deferred.reject();
+                        }
+                    });
+
+                    ui.showDialog(self.$dialog);
+                });
+            };
+        }
+    });
+}));
