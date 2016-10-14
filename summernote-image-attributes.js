2c7672cb38996a466aba1c5a4aa16923449fc7ec
Göktuğ ÖZTÜRK <goktug.ozturk@teknomavi.com>
2016-10-14 12:12:37 +0300

    Turkish Translation

--- summernote-image-attributes.js
+++ summernote-image-attributes.js
@@ -213,6 +213,40 @@
                 relSearch:'Search: Link zur Dokumentsuche',
                 relTag:'Tag: Ein Schl&uuml;sselwort (keyword) f&uuml;r diese Seite'
             }
+        },
+        'tr-TR':{ /* Turkish */
+            imageAttributes:{
+                dialogTitle:'Resim Özellikleri',
+                tooltip:'Resim Özellikleri',
+                tooltipShape:'Resim Şekli',
+                tooltipShapeOptions:['Responsive','Yuvarlatılmış','Daire','Önİzleme','Hiçbiri'],
+                pluginImageTitle:'Resim Özellikleri',
+                pluginLinkTitle:'Bağlantı Özellikleri',
+                title:'Başlık',
+                alt:'Alt. Metin',
+                class:'Sınıf',
+                classSelect:'Sınıf Seçin',
+                style:'Stil',
+                role:'Rol',
+                href:'URL',
+                target:'Hedef',
+                linkClass:'Bağlantı Sınıfı',
+                linkRole:'Rol',
+                rel:'Bağlantı İlişkisi(Rel)',
+                relBlank:'İlişki özelliğini kullanma',
+                relAlternate:'Alternatif: Belgenin farklı bir versiyonuna bağlantı',
+                relAuthor:'Yazar: Belgenin yazarına bağlantı',
+                relBookmark:'Yer İmi: Yer İmi eklemek için kalıcı adres',
+                relHelp:'Yardım: Yardım dökümanına giden bağlantı',
+                relLicense:'Lisans: Belgenin telif hakkı bilgisine bağlantı',
+                relNext:'Sonraki: Sıradaki belgeye giden bağlantı',
+                relNofollow:'Takip Etme (NoFollow): Arama motorlarının bu bağlantıyı takip etmemesini sağlar',
+                relNoreferrer:'Referanssız (NoReferrer): Tarayıcının bu bağlantıya referans adresi göndermemesi gerektiğini belirtir',
+                relPrefetch:'ÖnBellek(PreFetch): Hedef bağlantının ön belleğe alınması gerektiğini belirtir',
+                relPrev:'Önceki: Önceki belgeye giden bağlantı',
+                relSearch:'Arama: Belge için bir arama aracına bağlantı olduğunu belirtir.',
+                relTag:'Etiket: Belge için bir etiket olduğunu belirtir'
+            }
         }
     });
     $.extend($.summernote.options,{
